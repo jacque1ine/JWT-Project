3d54f743bb25b28c932645eb9a284760898e8239
Jacqueline Fung <fungjsl@gmail.com>
2022-12-19 22:17:07 -0500

    environment variables set up

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log('Hello World')
+const express = require('express') //backend
+const dotenv = require('dotenv').config(); //allow us to have a dotenv file wiht our variables in it 
+const port =  proccess.env.PORT || 5000 //gets PORT variable from .env file and sets that to the port we want our server to run on. 
+//^^will run on port 5000 IF PORT variable in .env file is not found
+
+const app = express()
+
+app.listen(port, () => console.log(`Sever started on port ${port}`))
